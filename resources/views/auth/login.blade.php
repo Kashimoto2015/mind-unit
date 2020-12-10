999b3060678e3c5cb92115810b61cf4792f2ada6
Kashimoto2015 <sanel.ts@gmail.com>
2020-12-10 21:08:56 +0100

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,140 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <title> Mind-Unit - Login</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" />
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!--Core CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/bulma.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link id="theme-sheet" rel="stylesheet" href="{{ asset('assets/css/night.css') }}">
+</head>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<body>
+<div class="pageloader"></div>
+<div class="infraloader is-active"></div>
+<!-- Wrapper -->
+<div class="login-wrapper columns is-gapless">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <!-- Form section -->
+    <div class="column is-7">
+        <div class="hero is-fullheight">
+            <div class="hero-heading">
+                <div class="auth-logo">
+                    <a href="/"><img class="top-logo switcher-logo" src="{{ asset('assets/img/logos/logo/bulkit-core.svg') }}" alt=""></a>
+                </div>
+            </div>
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns">
+                        <div class="column"></div>
+                        <div class="column is-5">
+
+                            <div class="auth-content">
+                                <h2>Welcome Back.</h2>
+                                <p>Please sign in to your account</p>
+                                <a href="/register">I do not have an account yet </a>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <!-- Login Form -->
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
+                                <div id="signin-form" class="login-form animated preFadeInLeft fadeInLeft">
+                                    <!-- Input -->
+                                    <div class="field pb-10">
+                                        <div class="control has-icons-right">
+                                            <input id="email" type="email" class="input is-medium has-shadow @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                            <span class="icon is-medium is-right">
+                                                        <i class="sl sl-icon-user"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <!-- Input -->
+                                    <div class="field pb-10">
+                                        <div class="control has-icons-right">
+                                            <input id="password" type="password" class="input is-medium has-shadow @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Passwort">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                            <span class="icon is-medium is-right">
+                                                        <i class="sl sl-icon-lock"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <!-- Submit -->
+                                    <p class="control login">
+                                        <button type="submit" class="button button-cta primary-btn btn-align-lg is-bold is-fullwidth rounded raised no-lh">
+                                            {{ __('Login') }}
+                                        </button>
+                                    </p>
                                 </div>
+                            </form>
+
+                            <!-- Toggles -->
+                            <div id="recover" class="pt-10 pb-10 forgot-password animated preFadeInLeft fadeInLeft">
+                                <p class="has-text-centered">
+                                    <a href="#">Forgot password ?</a>
+                                </p>
+                            </div>
+
+                            <div id="back-to-login" class="pt-10 pb-10 forgot-password animated preFadeInLeft fadeInLeft is-hidden">
+                                <p class="has-text-centered">
+                                    <a href="#">Back to Sign in</a>
+                                </p>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        <div class="column"></div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Image section (hidden on mobile) -->
+    <div class="column login-column is-5 is-hidden-mobile hero-banner">
+        <div class="hero login-hero is-fullheight is-theme-primary is-relative">
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    <!-- /Image section -->
 </div>
-@endsection
+<!-- /Wrapper -->
+<!-- Back To Top Button -->
+<div id="backtotop"><a href="#"></a></div> <!-- Concatenated jQuery and plugins -->
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+<!-- Bulkit js -->
+<script src="{{ asset('assets/js/functions.js') }}"></script>
+<script src="{{ asset('assets/js/auth.js') }}"></script>
+<script src="{{ asset('assets/js/contact.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+<!-- Landing page js -->
+</body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,201 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Required Meta Tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title> Mind-Unit - Home</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" />
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!--Core CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/bulma.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link id="theme-sheet" rel="stylesheet" href="{{ asset('assets/css/night.css') }}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+
+<body class="is-theme-core">
+<div class="pageloader"></div>
+<div class="infraloader is-active"></div>
+<!-- Hero and nav -->
+<div class="hero is-cover is-relative is-fullheight is-default is-bold">
+
+    <nav class="navbar navbar-wrapper navbar-fade navbar-light is-transparent">
+        <div class="container">
+            <!-- Brand -->
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <img class="light-logo" src="{{ asset('assets/img/logos/logo/bulkit-white.svg') }}" alt="">
+                    <img class="dark-logo switcher-logo" src="{{ asset('assets/img/logos/logo/bulkit-core.svg') }}" alt="">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                <!-- Responsive toggle -->
+                <div class="custom-burger" data-target="">
+                    <a id="" class="responsive-btn" href="javascript:void(0);">
+                            <span class="menu-toggle">
+                                <span class="icon-box-toggle">
+                                    <span class="rotate">
+                                        <i class="icon-line-top"></i>
+                                        <i class="icon-line-center"></i>
+                                        <i class="icon-line-bottom"></i>
+                                    </span>
+                            </span>
+                            </span>
+                    </a>
+                </div>
+                <!-- /Responsive toggle -->
+            </div>
 
-                    </ul>
+            <!-- Navbar menu -->
+            <div class="navbar-menu">
+                <!-- Navbar Start -->
+                <div class="navbar-start">
+                    <!-- Navbar item -->
+                    <a class="navbar-item is-slide" href="/about-us">
+                        About Us
+                    </a>
+                    <!-- Navbar item -->
+                    <a class="navbar-item is-slide" href="/products">
+                        Products
+                    </a>
+                    <!-- Navbar item -->
+                    <a class="navbar-item is-slide" href="/register">
+                        Register
+                    </a>
+                </div>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                <!-- Navbar end -->
+                <div class="navbar-end">
+                    <!-- Signup button -->
+                    <div class="navbar-item">
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <a id="#signup-btn" href="/login" class="button button-signup btn-outlined is-bold btn-align light-btn rounded raised">
+                                Login
+                            </a>
                         @endguest
+                        @auth
+                                <div class="button button-signup btn btn-outlined is-bold btn-align light-btn rounded raised is-drop">{{ auth()->user()->name }} <i class="sl sl-icon-arrow-down is-icon-xs ml-2"></i>
+                                    <div class="dropContain">
+                                        <div class="dropOut">
+                                            <ul>
+                                                <li><a href="/credits" style="text-decoration: none;"><i class="drop-icon sl sl-icon-wallet"></i> Guthaben: {{ auth()->user()->credit }}€</a></li>
+                                                <li>
+                                                    <a href="{{ route('logout') }}" style="text-decoration: none;" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="drop-icon sl sl-icon-logout"></i> Logout</a>
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                        @csrf
+                                                    </form>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                        @endauth
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('main')
+</div>
+@yield('content')
+
+<!-- Dark footer -->
+<footer id="dark-footer" class="footer footer-dark">
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <div class="footer-column">
+                    <div class="footer-header">
+                        <h3>Product</h3>
+                    </div>
+                    <ul class="link-list">
+                        <li><a href="#">Discover features</a></li>
+                        <li><a href="#">CMS integration</a></li>
+                        <li><a href="#">Customers</a></li>
+                        <li><a href="#">Weekly sessions</a></li>
+                        <li><a href="#">Free trials and demo</a></li>
+                        <li><a href="#">What's next ?</a></li>
                     </ul>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+            <div class="column">
+                <div class="footer-column">
+                    <div class="footer-header">
+                        <h3>Company</h3>
+                    </div>
+                    <ul class="link-list">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">About security</a></li>
+                        <li><a href="#">User guide</a></li>
+                        <li><a href="#">Terms of service</a></li>
+                        <li><a href="#">Terms of website use</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <div class="footer-column">
+                    <div class="footer-header">
+                        <h3>Learning</h3>
+                    </div>
+                    <ul class="link-list">
+                        <li><a href="#">Resources</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">API documentation</a></li>
+                        <li><a href="#">Admin guide</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <div class="footer-column">
+                    <div class="footer-logo">
+                        <img class="switcher-logo-w" src="{{ asset('assets/img/logos/logo/bulkit-core-w.svg') }}" alt="">
+                    </div>
+                    <div class="footer-header">
+                        <nav class="level is-mobile">
+                            <div class="level-left level-social">
+                                <a href="#" class="level-item">
+                                    <span class="icon"><i class="fa fa-facebook"></i></span>
+                                </a>
+                                <a href="#" class="level-item">
+                                    <span class="icon"><i class="fa fa-twitter"></i></span>
+                                </a>
+                                <a href="#" class="level-item">
+                                    <span class="icon"><i class="fa fa-linkedin"></i></span>
+                                </a>
+                                <a href="#" class="level-item">
+                                    <span class="icon"><i class="fa fa-dribbble"></i></span>
+                                </a>
+                                <a href="#" class="level-item">
+                                    <span class="icon"><i class="fa fa-github"></i></span>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="copyright">
+                        <span class="moto light-text">Designed and coded with <i class="fa fa-heart color-red"></i> by CSS Ninja.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</footer>
+<!-- /Dark footer -->
+<!-- Back To Top Button -->
+<div id="backtotop"><a href="#"></a></div>
+<!-- Google maps api call with api key -->
+<script src="https://maps.google.com/maps/api/js?key=AIzaSyAGLO_M5VT7BsVdjMjciKoH1fFJWWdhDPU"></script>
+<!-- Concatenated jQuery and plugins -->
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+<!-- Bulkit js -->
+<script src="{{ asset('assets/js/functions.js') }}"></script>
+<script src="{{ asset('assets/js/auth.js') }}"></script>
+<script src="{{ asset('assets/js/contact.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
 </html>
