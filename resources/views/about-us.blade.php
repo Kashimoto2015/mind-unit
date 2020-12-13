404f178dbbf0d4232d2a40226d835b45541823e9
Kashimoto2015 <sanel.ts@gmail.com>
2020-12-13 20:43:41 +0100

    Logo Design update

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -8,13 +8,13 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <title> Bulkit :: About</title>
-    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" />
 
     <!--Core CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bulma.css">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link id="theme-sheet" rel="stylesheet" href="assets/css/core.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bulma.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link id="theme-sheet" rel="stylesheet" href="{{ asset('assets/css/night.css') }}">
 
 </head>
 
