c74ff9ff4258d9729f9532ea9e9a99e1f1403dba
Kashimoto2015 <sanel.ts@gmail.com>
2020-12-14 08:45:03 +0100

    register update

--- resources/views/rechtliches/impressum.blade.php
+++ resources/views/rechtliches/impressum.blade.php
@@ -35,53 +35,53 @@
                     </div>
                 </div>
             </div>
-    </div>
-    <!-- FAQ -->
-    <section class="section is-medium section-feature-grey">
-        <div class="container">
+        <!-- FAQ -->
+        <section class="section is-medium section-feature-grey">
+            <div class="container">
 
-            <h2 class="title section-subtitle dark-text text-bold has-text-centered is-3 pt-20 pb-20">
-                You have some questions ?
-            </h2>
+                <h2 class="title section-subtitle dark-text text-bold has-text-centered is-3 pt-20 pb-20">
+                    You have some questions ?
+                </h2>
 
-            <div class="content-wrapper">
-                <div class="columns">
-                    <div class="column is-4 is-offset-2">
-                        <div class="content">
-                            <p class="text-bold dark-text">1. How do i get started?</p>
-                            <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
-                                nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
-                                consectetuer.</p>
+                <div class="content-wrapper">
+                    <div class="columns">
+                        <div class="column is-4 is-offset-2">
+                            <div class="content">
+                                <p class="text-bold dark-text">1. How do i get started?</p>
+                                <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
+                                    nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
+                                    consectetuer.</p>
+                            </div>
+                            <div class="content">
+                                <p class="text-bold dark-text">3. Where can i get training?</p>
+                                <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
+                                    nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
+                                    consectetuer.</p>
+                            </div>
+                            <div class="content">
+                                <p class="text-bold dark-text">5. Are updates mandatory?</p>
+                                <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
+                                    nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
+                                    consectetuer.</p>
+                            </div>
                         </div>
-                        <div class="content">
-                            <p class="text-bold dark-text">3. Where can i get training?</p>
-                            <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
-                                nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
-                                consectetuer.</p>
-                        </div>
-                        <div class="content">
-                            <p class="text-bold dark-text">5. Are updates mandatory?</p>
-                            <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
-                                nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
-                                consectetuer.</p>
-                        </div>
-                    </div>
-                    <div class="column is-4">
-                        <div class="content">
-                            <p class="text-bold dark-text">2. How can i add people to my team?</p>
-                            <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
-                                nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
-                                consectetuer.</p>
-                        </div>
-                        <div class="content pb-40">
-                            <p class="text-bold dark-text">4. Do you have a refund policy?</p>
-                            <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
-                                nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
-                                consectetuer.</p>
+                        <div class="column is-4">
+                            <div class="content">
+                                <p class="text-bold dark-text">2. How can i add people to my team?</p>
+                                <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
+                                    nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
+                                    consectetuer.</p>
+                            </div>
+                            <div class="content pb-40">
+                                <p class="text-bold dark-text">4. Do you have a refund policy?</p>
+                                <p>Lorem ipsum dolor sit amet, accusata voluptatibus per eu, probo summo argumentum ea vel. Pri
+                                    nonumy sententiae ex, eam adhuc regione tibique te. Et sit alii vero harum, ne his viderer
+                                    consectetuer.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 @endsection
