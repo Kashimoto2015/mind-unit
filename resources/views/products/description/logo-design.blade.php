082b010b154b395a1b65b19396e0822e67d94811
Kashimoto2015 <sanel.ts@gmail.com>
2020-12-16 16:28:34 +0100

    Logo Design update

--- resources/views/products/description/logo-design.blade.php
+++ resources/views/products/description/logo-design.blade.php
@@ -203,10 +203,9 @@
                             <label>Message *</label>
                         </div>
                         <div class="column"></div>
-                        <div id="uploader" class="section-wrapper">
-                            <div class="uploader-controls has-text-centered animated preFadeInUp fadeInUp">
-                                <input type="file" name="dragndrop">
-                            </div>
+                        <div class="uploader-controls animated preFadeInUp fadeInUp">
+                            <input type="file" name="fielduploader">
+                            <button type="submit" class="button btn-align button-cta raised no-lh is-bold secondary-btn">Upload</button>
                         </div>
                         <div class="column"></div>
                         <div class="mt-20 has-text-right">
